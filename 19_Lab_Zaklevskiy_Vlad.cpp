9bcbff707298827627d66c4931afb2d31993ba3d
Python-is-a-snake <45516701+Python-is-a-snake@users.noreply.github.com>
2020-10-20 11:30:29 +0300

    Update 19_Lab_Zaklevskiy_Vlad.cpp

--- 19_Lab_Zaklevskiy_Vlad.cpp
+++ 19_Lab_Zaklevskiy_Vlad.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-//Функція яка перевіряє чи є число досконалим
+//A function that checks whether a number is perfect
 bool is_perfect(int num)
 {
     int sum = 0;
     for (int d = num / 2; d >= 1; --d)
         if (num % d == 0)
             sum += d;
-    return sum == num;
+    return sum == num;//Return True or False
 }
 
 int main()
 {
-    setlocale(LC_ALL, "Ukrainian");
     int num;
-    cout << "Введіть число: ";
+    cout << "Enter number: ";
     cin >> num;
-    cout << "Число " << (is_perfect(num) ? "є" : "не є ") << "досконалим" << endl;
+    cout << "Number " << (is_perfect(num) ? "is" : "is not ") << "perfect" << endl;
     return 0;
 }
